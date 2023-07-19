3d502d0e8fbed966e20ba60479e0cb0117ee775b
Korvo2irow <82429011+Korvo2Tuirow@users.noreply.github.com>
2023-07-18 23:08:39 -0300

    segurancadeleteadm

--- excluir.php
+++ excluir.php
@@ -4,7 +4,7 @@ include "conn.php";
 
 $id = $_POST['id'];
 
-$sql = "DELETE FROM pessoas WHERE cod_pessoa = '$id'";
+$sql = "DELETE FROM pessoas WHERE cod_pessoa = '$id' AND adm != '1'";
 $sqli = mysqli_query($conn,$sql);
 if($sqli){
    header('location: pesquisa.php');
